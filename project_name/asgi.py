77defc3ab8bdd6719066febd44dce57f3bb8945e
Ivlev Denis <di-erz@yandex.ru>
2017-03-18 11:57:59 +0300

    Add ASGI and routing files

---
+++ project_name/asgi.py
@@ -0,0 +1,31 @@
+"""
+ASGI config for {{ project_name }} project.
+It exposes the ASGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://channels.readthedocs.io/en/stable/deploying.html#run-interface-servers
+"""
+import os
+import dotenv
+import channels.asgi
+
+
+try:
+    dotenv.read_dotenv(
+        os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+except Exception as e:
+    print(e)
+
+
+ENVIRONMENT = os.getenv('ENVIRONMENT')
+
+if ENVIRONMENT == 'STAGING':
+    settings = 'staging'
+elif ENVIRONMENT == 'PRODUCTION':
+    settings = 'production'
+else:
+    settings = 'development'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', settings.title())
+
+channel_layer = channels.asgi.get_channel_layer()
---
+++ project_name/routing.py
@@ -0,0 +1,8 @@
+"""
+Channels routing for {{ project_name }} project.
+For more information on this file, see
+https://channels.readthedocs.io/en/stable/routing.html
+"""
+from channels.routing import route, route_class
+
+channel_routing = []
