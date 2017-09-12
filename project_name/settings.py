87891f51995577dc4bffaa8a917f79f7571baa6d
Ivlev Denis <di-erz@yandex.ru>
2017-09-12 08:50:42 +0300

    Merge from upstream

--- project_name/settings.py
+++ project_name/settings.py
@@ -35,6 +35,8 @@ class Common(Configuration):
         # 3rd party apps
         'django_extensions',
         'debug_toolbar',
+
+        '{{ project_name }}.users',
     ]
 
     MIDDLEWARE = [
@@ -103,6 +105,8 @@ class Common(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/{{ docs_version }}/howto/static-files/
     STATIC_URL = '/static/'
+
+    AUTH_USER_MODEL = 'users.User'
 
 
 class Development(Common):
